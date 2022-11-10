dd257ae15f4fd14d819d6497ee0a98900806e2e8
Kowshic Ahmed Akash <kowshicahmed@yahoo.com>
2022-11-10 21:36:52 +0100

    hackerrank examples

--- DataStruct/src/Java1DArray.java
+++ DataStruct/src/Java1DArray.java
@@ -1,27 +1,20 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
-
-
 
 public class Java1DArray {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
+    public static void main(String[] args) {
 
-        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                .map(Integer::parseInt)
-                .collect(toList());
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++) {
+            a[i] = scan.nextInt();
+        }
+        scan.close();
 
-        bufferedReader.close();
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
     }
 }
