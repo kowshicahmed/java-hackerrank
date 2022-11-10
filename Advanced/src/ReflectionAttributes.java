e1eda3ef709626ab136acda353854a80ef350ff4
Kowshic Ahmed Akash <kowshicahmed@yahoo.com>
2022-11-10 16:39:31 +0100

    reflection attributes example

---
+++ Advanced/src/ReflectionAttributes.java
@@ -0,0 +1,47 @@
+import java.lang.reflect.*;
+import java.util.*;
+
+class Student{
+    private String name;
+    private String id;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
+class ReflectionAttributes {
+
+    public static void main(String[] args){
+        Class student = Student.class;
+        Method[] methods = Student.class.getDeclaredMethods();
+
+        ArrayList<String> methodList = new ArrayList<>();
+        for(Method method: methods){
+            methodList.add(method.getName());
+        }
+        Collections.sort(methodList);
+        for(String name: methodList){
+            System.out.println(name);
+        }
+    }
+
+}
+
+
